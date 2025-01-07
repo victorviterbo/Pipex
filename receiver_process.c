706e4b58e24030d62382333d49beb8f7296af01f
Victor Luc Etienne Viterbo <vviterbo@c1r16s7.42lausanne.ch>
2025-01-07 18:19:42 +0100

    improved error handling and messaging

--- receiver_process.c
+++ receiver_process.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 18:01:26 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/01/07 17:09:16 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:18:47 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,16 @@ void	receiver_process(char *outfname, int pipe_fd[], char *cmd, char **envp)
 	int		fd;
 
 	args = ft_cmd_parser(cmd);
+	if (!args)
+		ft_perror_exit("Receiver process command could not be parsed");
 	executable = find_execpath(args[0], envp);
 	if (!executable)
-		ft_perror_exit("Receiver cannot find executable");
+		ft_perror_exit("Receiver process cannot find executable");
 	if (!args)
 		return ;
 	fd = open(outfname, O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (!fd)
+		ft_perror_exit("Sender open failed input file");
 	if (dup2(pipe_fd[READ_END], STDIN_FILENO) == -1)
 		ft_perror_exit("Receiver dup2 failed on pipe");
 	if (dup2(fd, STDOUT_FILENO) == -1)
--- sender_process.c
+++ sender_process.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 18:01:14 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/01/07 17:09:16 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/01/07 18:18:42 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,14 @@ void	sender_process(char *infname, int pipe_fd[], char *cmd, char **envp)
 	int		fd;
 
 	args = ft_cmd_parser(cmd);
+	if (!args)
+		ft_perror_exit("Sender process command could not be parsed");
 	executable = find_execpath(args[0], envp);
 	if (!executable)
-		ft_perror_exit("Sender cannot find executable");
-	if (!args)
-		return ;
+		ft_perror_exit("Sender process cannot find executable");
 	fd = open(infname, O_RDONLY);
+	if (!fd)
+		ft_perror_exit("Sender open failed input file");
 	if (dup2(pipe_fd[WRITE_END], STDOUT_FILENO) == -1)
 		ft_perror_exit("Sender dup2 failed on pipe");
 	if (dup2(fd, STDIN_FILENO) == -1)
