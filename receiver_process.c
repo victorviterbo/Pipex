617815fc54de17a7ebfb4ab09770165bb3192f75
vviterbo <victor.viterbo@epfl.ch>
2025-01-15 13:16:27 +0100

    fixed error msg

--- receiver_process.c
+++ receiver_process.c
@@ -6,7 +6,7 @@
 /*   By: vviterbo <vviterbo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 18:01:26 by vviterbo          #+#    #+#             */
-/*   Updated: 2025/01/07 20:30:58 by vviterbo         ###   ########.fr       */
+/*   Updated: 2025/01/15 13:16:18 by vviterbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	receiver_process(char *outfname, int pipe_fd[], char *cmd, char **envp)
 		ft_perror_exit("Receiver process cannot find executable");
 	fd = open(outfname, O_CREAT | O_WRONLY | O_TRUNC, 0777);
 	if (fd < 0)
-		ft_perror_exit("Sender open failed input file");
+		ft_perror_exit("Receiver open failed ouput file");
 	if (dup2(pipe_fd[READ_END], STDIN_FILENO) == -1)
 		ft_perror_exit("Receiver dup2 failed on pipe");
 	if (dup2(fd, STDOUT_FILENO) == -1)
